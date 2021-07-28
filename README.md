f53778552199864841caf2445740c7e3f73616e7
Shreshth Kaushik <shreshthsoccer12@gmail.com>
2021-07-28 21:55:25 +0530

    Day1 completed and I added __name == __main__ here

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # FlaskBlog
 This is my flask blog application being tracked by git. 
 
+Day - 1) Completed my first day of learning flask and created routes and got rid of using envirnment vairables. I instead used __name__ == __main__ to run flask server directly using python.
+ 
--- flask_blog.py
+++ flask_blog.py
@@ -1,3 +1,15 @@
-#. venv/bin/activate
-import flask
-print("Hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def homePage():
+    return "<h1>This is my homepage</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>This is my about page</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
