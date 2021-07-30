91b92b447459ae82c452348e84f29c49f3d32981
Shreshth Kaushik <shreshthsoccer12@gmail.com>
2021-07-30 20:52:53 +0530

    Completed the forms and templete for my registration template

--- flask_blog.py
+++ flask_blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -28,10 +28,12 @@ def homePage():
 def about():
     return render_template("about.html", title = 'ABOUTPAGE')
 
-@app.route("/register")
+@app.route("/register", methods = ['GET', 'POST'])
 def register():
     form = RegistrationForm()
-
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('homePage'))
     return render_template("register.html", title = 'REGISTRATION PAGE', form = form)
 
 @app.route("/login")
