710d47ef3d3ec74074e4a738cc5c73eb6f5941d4
Shreshth Kaushik <shreshthsoccer12@gmail.com>
2021-07-30 19:56:49 +0530

    Created my Registration and Login forms

--- flask_blog.py
+++ flask_blog.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '04106331e37053a7'
 data = [
     {'Name' : 'Shreshth',
     'age' : 21,
@@ -26,6 +27,18 @@ def homePage():
 @app.route("/about")
 def about():
     return render_template("about.html", title = 'ABOUTPAGE')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+
+    return render_template("register.html", title = 'REGISTRATION PAGE', form = form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+
+    return render_template("login.html", title = 'LOGIN PAGE', form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,1 +1,16 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min = 2, max = 20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
+    
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Stay signed in ?')
+    submit = SubmitField('Log in')
