b7c9a268bb6ddfd369a351a82b64731da2f0da8a
Shreshth Kaushik <shreshthsoccer12@gmail.com>
2021-07-30 22:17:56 +0530

    Finished with login page completely

--- flask_blog.py
+++ flask_blog.py
@@ -36,10 +36,15 @@ def register():
         return redirect(url_for('homePage'))
     return render_template("register.html", title = 'REGISTRATION PAGE', form = form)
 
-@app.route("/login")
+@app.route("/login",methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
-
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == '123':
+            flash(f'Successful login!', 'success')
+            return redirect(url_for('homePage'))
+        else:
+            flash(f'Wrong credentials, try again', 'danger')
     return render_template("login.html", title = 'LOGIN PAGE', form = form)
 
 if __name__ == "__main__":
